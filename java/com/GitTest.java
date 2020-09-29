0c915373a12f7ed6a471e1c38158dbce85be89b9
lagrange <15238636501@163.com>
2020-09-29 15:07:08 +0800

    输出hello world

---
+++ java/com/GitTest.java
@@ -0,0 +1,12 @@
+package com;
+
+/***
+ * @author zhengyu.shang@hand-china.com 2020/09/29 15:05
+ *
+ *
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
