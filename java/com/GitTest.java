e21a83eddd5a0af6998dd0f2c0a37fa1b3f2c048
lagrange <15238636501@163.com>
2021-01-21 13:48:53 +0800

    11

--- java/com/GitTest.java
+++ java/com/GitTest.java
@@ -12,7 +12,7 @@ public class GitTest {
      */
     public static void main(String[] args) {
         System.out.println("What is the fuck!");
-        System.out.println(1);
+        System.out.println(11);
         System.out.println(2);
     }
 
