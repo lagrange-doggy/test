83fb5d07c8403218e4af5c1dd1cfd6babe87a124
lagrange <15238636501@163.com>
2021-03-01 21:37:19 +0800

    sdfa

--- java/com/GitTest.java
+++ java/com/GitTest.java
@@ -13,7 +13,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("What is the fuck!");
         System.out.println(1);
-        System.out.println(22);
+        System.out.println(2);
     }
 
     ////////////////
